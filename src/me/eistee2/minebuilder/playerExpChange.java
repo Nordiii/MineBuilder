09a106376d93a32cf338411a7096dbf1090e6bde
Eistee2 <niklas907@googlemail.com>
2012-08-17 02:02:52 +0200

    Fix MaxLvl

--- src/me/eistee2/minebuilder/playerExpChange.java
+++ src/me/eistee2/minebuilder/playerExpChange.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
+import org.bukkit.event.player.PlayerLevelChangeEvent;
 
 public class playerExpChange implements Listener {
 	
@@ -15,6 +16,14 @@ public class playerExpChange implements Listener {
 		if(event.getPlayer().getLevel() >= info.getMaxLevel())
 		{
 			event.setAmount(0);
+		}
+	}
+	@EventHandler(priority = EventPriority.NORMAL)
+	private void onPlayerLevelUp(PlayerLevelChangeEvent event)
+	{
+		if(event.getPlayer().getLevel() >= info.getMaxLevel())
+		{
+			event.getPlayer().setLevel(info.getMaxLevel());
 		}
 	}
 
