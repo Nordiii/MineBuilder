c56bb1e86a21281a0d271bc66247dfab180b3275
Nordiii <niklas907@gmail.com>
2019-12-28 00:40:48 +0100

    add StoredMaterial for mbadd command | remove matching method

--- src/main/java/de/models/PlayerWrapper.java
+++ src/main/java/de/models/PlayerWrapper.java
@@ -6,12 +6,17 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-class PlayerWrapper {
-    private Player player;
+public class PlayerWrapper {
+    private final Player player;
+    private final HashMap<Class, ArrayList<Exp>> configs = new HashMap<>();
+    private String storedMaterial;
+
+    public String getStoredMaterial() {
+        return storedMaterial;
+    }
 
     public boolean isAddFlag() {
-        if(addFlag)
-        {
+        if (addFlag) {
             toggleFlag();
             return true;
         }
@@ -23,9 +28,12 @@ class PlayerWrapper {
     }
 
     private boolean addFlag;
-    private HashMap<Class,ArrayList<Exp>> configs = new HashMap<>();
 
-    public void updateItem(Class event,Exp item){
+    public void setStoredMaterial(String storedMaterial) {
+        this.storedMaterial = storedMaterial;
+    }
+
+    public void updateItem(Class event, Exp item) {
         configs.get(event).remove(item);
         configs.get(event).add(item);
     }
@@ -37,17 +45,13 @@ class PlayerWrapper {
                 .forEach(event->configs.put(event.getClass(),new ArrayList<>()));
     }
 
-    public boolean matchPlayer(PlayerWrapper player) {
-        return this.player.getUniqueId() == player.player.getUniqueId();
-
-    }
-
     public <C> int getExp(Class<C> event,Exp item)
     {
         ArrayList<Exp> entities = configs.get(event);
 
         if(!entities.contains(item))
             entities.add(item);
+
         return entities.stream()
                 .filter(item::equals)
                 .mapToInt(Exp::getExp)
