8861909cb9fd2303c77c01904339e2bac8385755
Nordiii <niklas907@gmail.com>
2019-12-28 00:42:37 +0100

    add synchronized to prevent race conditions

--- src/main/java/de/protection/Protection.java
+++ src/main/java/de/protection/Protection.java
@@ -8,26 +8,24 @@ import java.util.ArrayList;
 
 public class Protection {
 
-    private static Protection instance = new Protection();
+    private static final Protection instance = new Protection();
 
     public static Protection getInstance(){
         return instance;
     }
 
 
-    private static ArrayList<Location> protectedBlocks = new ArrayList<>();
+    private static final ArrayList<Location> protectedBlocks = new ArrayList<>(400);
 
     private Protection() {
     }
 
-    public boolean isProtected(Block b)
-    {
+    public synchronized boolean isProtected(Block b) {
         return protectedBlocks.remove(b.getLocation());
     }
 
-    public void addProtection(Block b)
-    {
-        if(protectedBlocks.size()>=400)
+    public synchronized void addProtection(Block b) {
+        if (protectedBlocks.size() >= 400)
             protectedBlocks.remove(0);
         protectedBlocks.add(b.getLocation());
     }
